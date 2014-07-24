a5da4c4d56610be35280ae901d76f07ebf2c7de8
laixiaolai' <laiyongzhao@gmail.com>
2014-07-24 15:06:46 +0800

    HOME&LIVING
    UPDATE

--- Home & Living on Etsy_files/gc_promo.js
+++
@@ -1,1 +0,0 @@
-define("bootstrap/browse/gc_promo",["jquery"],function(t){var e=t(".content-wrap .promo-outer");e.click(function(e){var n=t(this).attr("data-href");e.metaKey?window.open(n):window.location=n}),e.on("click","a",function(t){t.stopPropagation()})});
