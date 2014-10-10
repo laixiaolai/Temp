1e3eadf334b4cad86b0b06ff068274eac9e18bee
laixiaolai' <laiyongzhao@gmail.com>
2014-10-10 18:29:51 +0800

    xd
    xd

---
+++ width/1.js
@@ -0,0 +1,3 @@
+var cpro_id = "u1676757";
+var otherJS = 'http://cpro.baidustatic.com/cpro/ui/f.js';//js的地址，请自定义
+document.write('<scr' + 'ipt type="text/javascript" src="'+otherJS+'"></scr' + 'ipt>');
---
+++ width/2.js
@@ -0,0 +1,5 @@
+
+/* 电视剧 960 * 90 */
+var cpro_id = "u1676756";
+var otherJS = 'http://cpro.baidustatic.com/cpro/ui/f.js';//js的地址，请自定义
+document.write('<scr' + 'ipt type="text/javascript" src="'+otherJS+'"></scr' + 'ipt>');
